8f0adc2687d7c89d5d8603d27cc19e113e201447
user <60804817+ichg4354@users.noreply.github.com>
2020-12-28 16:55:01 +0900

    finish function for preloading Fonts

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React, { useState } from "react"
 import { Text, Image } from "react-native"
 import AppLoading from "expo-app-loading"
 import { Asset } from "expo-asset"
-
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from "expo-font"
+import { loadAsync } from "expo-font";
 
 const casheImages = images => images.map(image => {
   if (typeof image === "string") {
@@ -12,11 +14,14 @@ const casheImages = images => images.map(image => {
   }
 })
 
+const casheFonts = fonts => fonts.map(font => Font.loadAsync(font))
+
 export default function App() {
   const [isLoading, setIsLoading] = useState(false)
-  const loadAssets = async () => {
+  const loadAssets = () => {
     const images = casheImages(['https://unsplash.com/photos/1gLdTsX3_70'])
-    console.log(images)
+    const fonts = casheFonts([Ionicons.font])
+    return Promise.all([...images, ...fonts])
   }
   const onFinish = () => setIsLoading(true)
   return isLoading ? <Text>LOADED!!</Text> : <AppLoading startAsync={loadAssets} onFinish={onFinish} onError={console.error} />
