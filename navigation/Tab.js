90684422c9b3440ee1bbc1fe16a53f193c1f4e4d
user <60804817+ichg4354@users.noreply.github.com>
2020-12-29 18:24:08 +0900

    finish my way of changing headers via tab navigate

--- navigation/Tab.js
+++ navigation/Tab.js
@@ -9,9 +9,17 @@ import Favs from "../screens/Favs";
 
 const Tab = createBottomTabNavigator()
 
-export default () => <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen name="TV" component={Tv} />
-    <Tab.Screen name="Search" component={Search} />
-    <Tab.Screen name="Favs" component={Favs} />
-</Tab.Navigator>
+export default (object) => {
+    object.navigation.setOptions({ title: "Movies" })
+    if (object.route.state) {
+        const routeNames = object.route.state.routeNames
+        const routeIndex = object.route.state.index
+        object.navigation.setOptions({ title: routeNames[routeIndex] })
+    }
+    return (<Tab.Navigator>
+        <Tab.Screen name="Movies" component={Movies} />
+        <Tab.Screen name="TV" component={Tv} />
+        <Tab.Screen name="Search" component={Search} />
+        <Tab.Screen name="Favs" component={Favs} />
+    </Tab.Navigator>)
+}
