db8fe70c4dd2ca904e60ace7dab8db7b36391b40
user <60804817+ichg4354@users.noreply.github.com>
2021-01-05 16:09:38 +0900

    create api handle on Tv.js

--- screens/Tv.js
+++ screens/Tv.js
@@ -1,4 +1,32 @@
-import React from "react"
-import { View, Text } from "react-native"
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { tvApi } from "../api";
 
-export default () => <View><Text>TV</Text></View> 
+export default () => {
+  const [tv, setTv] = useState({});
+  const getData = async () => {
+    const [today, todayError] = await tvApi.today();
+    const [thisWeek, thisWeekError] = await tvApi.thisWeek();
+    const [topRated, topRatedError] = await tvApi.topRated();
+    const [popular, popularError] = await tvApi.popular();
+    setTv({
+      today,
+      todayError,
+      thisWeek,
+      thisWeekError,
+      topRated,
+      topRatedError,
+      popular,
+      popularError,
+    });
+  };
+  console.log(tv.length);
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <View>
+      <Text>{tv.today?.length}</Text>
+    </View>
+  );
+};
