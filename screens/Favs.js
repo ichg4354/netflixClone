a5c50591c42dbf103e603930b1ec98f4bf8f51c1
user <60804817+ichg4354@users.noreply.github.com>
2021-01-05 16:13:51 +0900

    finish api handle on Favs.js

--- screens/Favs.js
+++ screens/Favs.js
@@ -1,4 +1,19 @@
-import React from "react"
-import { View, Text } from "react-native"
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { movieApi } from "../api";
 
-export default () => <View><Text>FAVS</Text></View> 
+export default () => {
+  const [movie, setMovie] = useState({});
+  const getData = async () => {
+    const [discover, discoverError] = await movieApi.discover();
+    setMovie({ discover, discoverError });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <View>
+      <Text>{movie.discover?.length}</Text>
+    </View>
+  );
+};
