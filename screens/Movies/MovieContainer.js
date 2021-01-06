04c27687fe540d3fbe5b369f766a94617154ca4f
user <60804817+ichg4354@users.noreply.github.com>
2021-01-06 20:07:06 +0900

    make slider and fill it with nowPlaying movie title

--- screens/Movies/MovieContainer.js
+++ screens/Movies/MovieContainer.js
@@ -4,7 +4,7 @@ import { movieApi } from "../../api";
 import MoviePresenter from "./MoviePresenter";
 
 export default () => {
-  const [movies, setMovies] = useState({});
+  const [movies, setMovies] = useState({ loading: true });
   const getData = async () => {
     const [nowPlaying, nowPlayingError] = await movieApi.nowPlaying();
     const [popular, popularError] = await movieApi.popular();
@@ -16,11 +16,12 @@ export default () => {
       popularError,
       upcomming,
       upcommingError,
+      loading: false,
     });
   };
 
   useEffect(() => {
     getData();
   }, []);
-  return <MoviePresenter></MoviePresenter>;
+  return <MoviePresenter {...movies} />;
 };
--- screens/Movies/MoviePresenter.js
+++ screens/Movies/MoviePresenter.js
@@ -1,8 +1,41 @@
 import React from "react";
-import { View, Text } from "react-native";
+import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
+import { ActivityIndicator, Dimensions } from "react-native";
 
-export default () => (
-  <View>
-    <Text>HI</Text>
-  </View>
+const { width, height } = Dimensions.get("screen");
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Header = styled.View`
+  width: 100%;
+  height: ${height / 3};
+  background-color: blue;
+`;
+
+const Text = styled.Text`
+  color: red;
+`;
+
+const Section = styled.View``;
+
+export default ({ loading, nowPlaying }) => (
+  <Container>
+    {loading ? (
+      <ActivityIndicator color="white" />
+    ) : (
+      <Header>
+        <Swiper loop timeout={3} controlsEnabled={false} autoPlay={true}>
+          {nowPlaying.map((each) => (
+            <Section key={each.id}>
+              <Text>{each.title}</Text>
+            </Section>
+          ))}
+        </Swiper>
+      </Header>
+    )}
+  </Container>
 );
